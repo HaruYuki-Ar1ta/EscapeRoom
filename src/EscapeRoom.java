7f5a97d22549e39d32d8aec8ddd85feb026f0cb3
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-12-03 13:20:35 -0500

    Instructions

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,6 +1,39 @@
 public class EscapeRoom {
 
+    /*
+    -options: Under the bed, Behind the desk, Under the desk, In both the desk drawers, In the bookshelf
+    -Behind the desk: small hole with a key in it
+    -On the desk is a computer: locked, 4 digit passcode,
+    -1st drawer: opens, empty
+    -2nd drawer: locked, keyhole
+    -1st Shelf: 2 books, one has a bookmark
+    -2nd shelf: empty
+    -3rd Shelf: A row of books on the left side, A mini cactus, Two picture frames one filled, A wooden miniturette, and 3 clay pieces
+
+    solution:
+    - player finds the key behind the desk
+    - player uses the key to open the 2nd desk drawer
+    - player finds a key card in the drawer
+    - player finds the passcode on the bookmark in the book on the bookshelf
+    - player unlocks the computer finding the code to the door
+
+    GUI:
+    - Big text box on the left
+    - three buttons moderately spaced buttons up-down
+    - Below the text box and the buttons, boxes with the inventory
+     */
+
+
+
+
+
+
     public static void main(String[] args) {
+
+
+
+
+
 
     }
 
